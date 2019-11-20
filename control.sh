24df2530afd809e4c69593ca9760fc9f1b6633f1
spaider77 <58012373+spaider77@users.noreply.github.com>
2019-11-21 00:17:01 +0100

    Update control.sh

--- control.sh
+++ control.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm /usr/bin/helloworld*
 touch /usr/bin/helloworld
 echo "#!/bin/bash">/usr/bin/helloworld
 echo 'echo "hello world"'>>/usr/bin/helloworld
+chmod +x /usr/bin/helloworld
