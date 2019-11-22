e594f04c5c2187f7d8b5af602007d79e3df423fd
spaider77 <58012373+spaider77@users.noreply.github.com>
2019-11-22 22:41:13 +0100

    Update control.sh

--- control.sh
+++ control.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-cd /home/x
-sudo rm /usr/bin/helloworld*
-sudo touch helloworld
-sudo echo "#!/bin/bash">helloworld
-sudo echo 'echo "hello world"'>>helloworld
-sudo chmod +x helloworld
-sudo mv helloworld /usr/bin/
-wg-quick down sweden1
-wg-quick down spaider-wg0
-wg-quick down niderland
-wg-quick down norway
-wg-quick down spain
-wg-quick down sweden2
-wg-quick down unitedkingdom
-wg-quick up www
+sudo wg-quick down sweden1
+sudo wg-quick down spaider-wg0
+sudo wg-quick down niderland
+sudo wg-quick down norway
+sudo wg-quick down spain
+sudo wg-quick down sweden2
+sudo wg-quick down unitedkingdom
+sudo wg-quick up www
