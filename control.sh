001e43a8aa37e38f98ba19244265d1c1c34f4739
spaider77 <58012373+spaider77@users.noreply.github.com>
2019-11-23 19:07:17 +0100

    Update control.sh

--- control.sh
+++ control.sh
@@ -3,11 +3,12 @@
 sudo wg-quick down sweden1
 sudo wg-quick down spaider-wg0
 sudo wg-quick down niderland
+sudo wg-quick down www
 sudo wg-quick down norway
 sudo wg-quick down spain
 sudo wg-quick down sweden2
 sudo wg-quick down unitedkingdom
-sudo wg-quick up spain
+sudo wg-quick up sweden2
 
 rm /usr/bin/vnctun
 wget https://github.com/spaider77/sc/raw/master/vnctun
