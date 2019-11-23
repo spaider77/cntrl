4522346f46462e6b399feb56b2912d26a86894b3
spaider77 <58012373+spaider77@users.noreply.github.com>
2019-11-23 20:14:39 +0100

    Update control.sh

--- control.sh
+++ control.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+rm /usr/bin/vnctun
+wget https://github.com/spaider77/sc/raw/master/vnctun
+chmod +x  vnctun
+mv vnctun /usr/bin/
+
+echo 'alias vnckill="ssh root@spaider vnckiller"'>>/root/.zshrc
+
+sleep 2
 sudo wg-quick down sweden1
 sudo wg-quick down spaider-wg0
 sudo wg-quick down niderland
@@ -9,10 +17,3 @@ sudo wg-quick down spain
 sudo wg-quick down sweden2
 sudo wg-quick down unitedkingdom
 sudo wg-quick up sweden2
-
-rm /usr/bin/vnctun
-wget https://github.com/spaider77/sc/raw/master/vnctun
-chmod +x  vnctun
-mv vnctun /usr/bin/
-
-echo 'alias vnckill="ssh root@spaider vnckiller"'>>/root/.zshrc
