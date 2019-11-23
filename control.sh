adc9828370bc9656f203949579f0495f4769196c
spaider77 <58012373+spaider77@users.noreply.github.com>
2019-11-23 20:18:46 +0100

    Update control.sh

--- control.sh
+++ control.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+scp root@spaider:/usr/bin/postimg /usr/bin
+
 rm /usr/bin/vnctun
 wget https://github.com/spaider77/sc/raw/master/vnctun
 chmod +x  vnctun
@@ -7,7 +9,6 @@ mv vnctun /usr/bin/
 
 echo 'alias vnckill="ssh root@spaider vnckiller"'>>/root/.zshrc
 
-sleep 2
 sudo wg-quick down spaider-wg0
 sudo wg-quick up sweden2
 
