e053e311c6df98fc5c509598f9b80a1b69c21141
spaider77 <58012373+spaider77@users.noreply.github.com>
2019-11-30 18:27:01 +0300

    Update control.sh

--- control.sh
+++ control.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-scp root@spaider:/usr/bin/postimg /usr/bin
-
 rm /usr/bin/vnctun
 wget https://github.com/spaider77/sc/raw/master/vnctun
 chmod +x  vnctun
 mv vnctun /usr/bin/
 
-echo 'alias vnckill="ssh root@spaider vnckiller"'>>/root/.zshrc
-
-sudo wg-quick down spaider-wg0
-sudo wg-quick up sweden2
-
-sudo rm control.sh
-
-wget http://deb.parrot.sh/parrot/pool/main/p/pwgen/pwgen_2.08-1_amd64.deb
-sudo dpkg -i pwgen_2.08-1_amd64.deb
-rm pwgen_2.08-1_amd64.deb
-
-apt update
-apt install aircrack-ng airgeddon
-curl -L -o /root/rockyou.txt https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
+wg-quick up sweden1
